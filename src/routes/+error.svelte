8bb0a35de716fb066c72387e61ebafcccbbe62c3
rts4 <mapfg.rts4@techhh.ca>
2023-07-04 16:20:53 -0400

    add error page - rts4

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+</script>
+
+<div class="container mx-auto my-5 alert alert-danger">
+    <strong>Error {$page.status}</strong>: {$page.error?.message}
+</div>
